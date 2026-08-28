9c8cb209081960d22d13b30dde3ea18b95130588
Yashwanth <170221536+BadakalaYashwanth@users.noreply.github.com>
2026-08-28 13:55:56 +0530

    Time: 4 ms (4.23%) | Memory: 19.1 MB (89.67%) - LeetSync

--- 2602-maximum-enemy-forts-that-can-be-captured/maximum-enemy-forts-that-can-be-captured.py
+++ 2602-maximum-enemy-forts-that-can-be-captured/maximum-enemy-forts-that-can-be-captured.py
@@ -5,14 +5,25 @@ class Solution:
 
         for right in range(len(forts)):
 
+            # If we find 0, it is an enemy fort.
+            # Keep expanding the window.
             if forts[right] == 0:
                 continue
 
+            # We reached 1 or -1.
+            # Check if the two ends are opposite:
+            # 1 ... -1  OR  -1 ... 1
             if (forts[left] == 1 and forts[right] == -1) or \
                (forts[left] == -1 and forts[right] == 1):
 
-                max_count = max(max_count, right - left - 1)
+                # Count the 0s between left and right.
+                current_count = right - left - 1
 
+                # Store the biggest group of 0s found so far.
+                max_count = max(max_count, current_count)
+
+            # Current 1 or -1 becomes the new left boundary.
             left = right
 
+        # Return the maximum enemy forts that can be captured.
         return max_count
