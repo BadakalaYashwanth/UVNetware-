b4384a7140cb4c9ddaafa941241941568d2db4d7
Yashwanth <170221536+BadakalaYashwanth@users.noreply.github.com>
2026-08-29 10:06:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (88.61%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,11 +1,20 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+
+        # slow points to the position where the next valid element should be placed
         slow = 0
 
-        for fast in range(len((nums))):
-                if nums[fast] != val:
-                    nums[slow] = nums[fast]
-                
-                    slow += 1
-        
+        # fast scans every element in the array
+        for fast in range(len(nums)):
+
+            # Keep the element if it is not equal to val
+            if nums[fast] != val:
+
+                # Copy the valid element to the slow position
+                nums[slow] = nums[fast]
+
+                # Move slow to the next position
+                slow += 1
+
+        # Return the number of valid elements
         return slow
