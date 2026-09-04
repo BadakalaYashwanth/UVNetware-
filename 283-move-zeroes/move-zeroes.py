55a42b51794c2df007a6ea16d87e84014bd3e76e
Yashwanth <170221536+BadakalaYashwanth@users.noreply.github.com>
2026-09-04 11:15:22 +0530

    Time: 3 ms (81.70%) | Memory: 20.4 MB (62.94%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,21 +1,15 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        # slow points to where the next non-zero value should go
-        slow = 0
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
 
-        # fast scans the entire array
-        for fast in range(len(nums)):
-
-            # If the current value is not zero, keep it
-            if nums[fast] != 0:
-
-                # Copy the non-zero value to the slow position
-                nums[slow] = nums[fast]
-
-                # Move slow to the next position
-                slow += 1
-
-        # After scanning the whole array, fill the remaining positions with 0
-        while slow < len(nums):
-            nums[slow] = 0
-            slow += 1
+        for right in range(len(nums)):
+            if nums[right] != 0:
+                nums[left] = nums[right]
+                left += 1
+        
+        while left < len(nums):
+            nums[left] = 0
+            left += 1
